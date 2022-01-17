37a2ac8009b87cfac248f5e43e6c1558d7a253c2
Nayiem <info@hodlercompany.com>
2022-01-18 00:22:18 +0100

    init

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Solar Network', csrfToken: req.csrfToken(), sessionId: req.session.id });
+});
+
+router.get('/faq', function(req, res, next) {
+    res.render('faq', { title: 'Solar Network', csrfToken: req.csrfToken(), sessionId: req.session.id });
+});
+
+router.get('/mobile-wallet', function(req, res, next) {
+    res.render('mobile-wallet', { title: 'Solar Network', csrfToken: req.csrfToken(), sessionId: req.session.id });
+});
+
+module.exports = router;
