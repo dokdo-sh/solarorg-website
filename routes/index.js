ad5dae63e2f8c841ba273b82292cd35d994fe19b
Nayiem <info@hodlercompany.com>
2022-01-18 01:54:08 +0100

    migrate to node express

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,17 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Solar Network', csrfToken: req.csrfToken(), sessionId: req.session.id });
 });
 
+/*
 router.get('/faq', function(req, res, next) {
     res.render('faq', { title: 'Solar Network', csrfToken: req.csrfToken(), sessionId: req.session.id });
 });
 
 router.get('/mobile-wallet', function(req, res, next) {
     res.render('mobile-wallet', { title: 'Solar Network', csrfToken: req.csrfToken(), sessionId: req.session.id });
+});
+*/
+router.get('/roadmap', function(req, res, next) {
+    res.render('roadmap', { title: 'Solar Network', csrfToken: req.csrfToken(), sessionId: req.session.id });
 });
 
 module.exports = router;
